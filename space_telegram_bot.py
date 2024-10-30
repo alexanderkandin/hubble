57830a7c2bcdeb0279b2563f42e5089ca74cc908
alexanderkandin <46374639+alexanderkandin@users.noreply.github.com>
2024-10-30 20:40:48 +0300

    Добавил скрипт для телеграм бота. Сейчас отправляет только текст

---
+++ space_telegram_bot.py
@@ -0,0 +1,5 @@
+import telegram
+
+bot = telegram.Bot(token='8141977417:AAFPSLDpTRtg6kyHGL1HxSwhTn8hs9Eo-78')
+
+bot.send_message(chat_id="@starsspacesuper", text="Привет! Это сообщение из Python.")
