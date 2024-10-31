9e7b66df3ca5d2da8987c23a832301c1a6ab613f
alexanderkandin <46374639+alexanderkandin@users.noreply.github.com>
2024-10-31 23:19:21 +0300

    Заменил переменный окружения и изменил цикл

--- fetch_EPIC_NASA_images.py
+++ fetch_EPIC_NASA_images.py
@@ -10,22 +10,20 @@ from dotenv import load_dotenv
 def main():
     try:
         load_dotenv()
-        api_key = os.getenv("API_KEY")
+        api_key = os.getenv("NASA_API_KEY")
         if not api_key:
-            raise ValueError("Переменная окружения 'API_KEY' не найдена.")
+            raise ValueError("Переменная окружения 'NASA_API_KEY' не найдена.")
         payload = {
             "api_key": api_key,
         }
         response = requests.get('https://api.nasa.gov/EPIC/api/natural/images', params=payload)
         response.raise_for_status()
         epic_images_data = response.json()
-        image_id = [id["image"] for id in epic_images_data]
-        image_date = [date["date"] for date in epic_images_data]
-        for image in range(0, len(image_date)):
-            date = str(image_date[image].split(" ")[0]).replace('-', '/')
-            id = image_id[image]
+        for count, image_and_data in enumerate(epic_images_data):
+            date = str(image_and_data['date'].split(" ")[0]).replace('-', '/')
+            id = image_and_data["image"]
             url = f'https://api.nasa.gov/EPIC/archive/natural/{date}/png/{id}.png?api_key=gdPzfbvhAsScM7wQyZRkUkMllJEmTzaS5Br4uPii'
-            download_image(url,"images",f'NASA_EPIC_{image}{get_links_ext(url)}')
+            download_image(url,"images",f'NASA_EPIC_{count}{get_links_ext(url)}')
     except requests.exceptions.HTTPError as http_err:
         print(f'HTTP ошибка: {http_err}')
 
