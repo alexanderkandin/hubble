6a3f04d271bb021e5843f41a20423d80aa3fa6f4
alexanderkandin <46374639+alexanderkandin@users.noreply.github.com>
2024-11-02 15:36:28 +0300

    Изменил название переменной

--- download_images.py
+++ download_images.py
@@ -3,10 +3,10 @@ import os
 
 
 
-def download_image(url_user, folder, name):
+def download_image(url, folder, name):
     save_path = os.path.join(os.getcwd(), folder, name)
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
-    response = requests.get(url_user)
+    response = requests.get(url)
     response.raise_for_status()
     with open(save_path, 'wb') as file:
         file.write(response.content)
