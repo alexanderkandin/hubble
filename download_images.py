91ff2087fed477708213bc517dd84f5d5fa0ab33
alexanderkandin <46374639+alexanderkandin@users.noreply.github.com>
2024-11-02 23:39:16 +0300

    Добавил аргумент payload

--- download_images.py
+++ download_images.py
@@ -3,10 +3,10 @@ import os
 
 
 
-def download_image(url, folder, name, payload):
+def download_image(url, folder, name, payload=None):
     save_path = os.path.join(os.getcwd(), folder, name)
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
-    response = requests.get(url,params=payload)
+    response = requests.get(url,params=payload) if payload else requests.get(url)
     response.raise_for_status()
     with open(save_path, 'wb') as file:
         file.write(response.content)
