18388cbf5b97c300f51594d9ecc52346883d6d93
alexanderkandin <46374639+alexanderkandin@users.noreply.github.com>
2024-11-02 16:52:54 +0300

    Добавил уведомление, если нет картинок при запросе, уточнил except исключение

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -10,16 +10,17 @@ def main():
         parser.add_argument("flight_id", nargs="?", help='Укажите номер полета.', default="latest")
         flight_id = parser.parse_args(sys.argv[1:]).flight_id
         if flight_id == "latest":
-            response = requests.get(f'https://api.spacexdata.com/v3/launches/{flight_id}')
-        else:
             response = requests.get(f'https://api.spacexdata.com/v5/launches/{flight_id}')
         response.raise_for_status()
         spacex_images = response.json()
         links = spacex_images['links']['flickr']['original']
-        for link_number, link in enumerate(links):
-            download_image(link, 'images', f'space_{link_number}{get_links_ext(link)}')
+        if not links:
+            print("В последних полетах нет фото")
+        else:
+            for link_number, link in enumerate(links):
+                download_image(link, 'images', f'space_{link_number}{get_links_ext(link)}')
     except requests.exceptions.HTTPError as http_err:
-        print(f'HTTP ошибка: {http_err}')
+        print(f'Введен неправильный номер полета: {http_err}')
 
 
 if __name__  == "__main__":
