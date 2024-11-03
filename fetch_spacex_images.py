afba4849b5835956446d395026ddcf92ba82b792
alexanderkandin <46374639+alexanderkandin@users.noreply.github.com>
2024-11-03 19:54:08 +0300

    Исправил исключения Исправил логику скрипта

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -13,6 +13,8 @@ def main():
         flight_id = parser.parse_args(sys.argv[1:]).flight_id
         if flight_id == "latest":
             response = requests.get(f'https://api.spacexdata.com/v5/launches/{flight_id}')
+        else:
+            response = requests.get(f'https://api.spacexdata.com/v5/launches/{flight_id}')
         response.raise_for_status()
         spacex_images = response.json()
         links = spacex_images['links']['flickr']['original']
@@ -22,7 +24,9 @@ def main():
             for link_number, link in enumerate(links):
                 download_image(link, 'images', f'space_{link_number}{get_links_ext(link)}')
     except requests.exceptions.HTTPError as http_err:
-        print(f'Введен неправильный номер полета: {http_err}')
+        print(f"Ошибка HTTP: {http_err}. Проверьте параметры запроса и доступность сервера.")
+    except requests.exceptions.ConnectionError:
+        print("Ошибка соединения. Проверьте подключение к интернету или доступность сервера.")
 
 
 if __name__  == "__main__":
